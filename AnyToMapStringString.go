03a5e00ca7b074ad9a932a39220e2c5b030536ad
Milan Lesichkov <lesichkovm@gmail.com>
2023-06-03 07:51:53 +0000

    Updates

--- AnyToMapStringString.go
+++ AnyToMapStringString.go
@@ -1,5 +1,37 @@
 package maputils
 
+import "reflect"
+
 func AnyToMapStringString(data any) map[string]string {
+	if data == nil {
+		return nil
+	}
+
+	t := reflect.TypeOf(data)
+
+	// Is it a map?
+	if t.Kind() != reflect.Map {
+		return nil
+	}
+
+	elemKeyKind := t.Key().Kind()
+	elemKValueKind := t.Elem().Kind()
+
+	// Is it a match?
+	if elemKeyKind == reflect.String && elemKValueKind == reflect.String {
+		return data.(map[string]string)
+	}
+
+	// Is it map[string]any? Convert to map[string]string
+	if elemKeyKind == reflect.String && elemKValueKind == reflect.Interface {
+		mapStringInterface := data.(map[string]interface{})
+		mapStringString := map[string]string{}
+		for key, value := range mapStringInterface {
+			mapStringString[key] = toString(value)
+		}
+		return mapStringString
+	}
+
+	// Last attempt to convert, may panic and need another condition
 	return data.(map[string]string)
 }
--- AnyToMapStringString_test.go
+++ AnyToMapStringString_test.go
@@ -19,3 +19,18 @@ func TestAnyToMapStringString(t *testing.T) {
 		t.Error("Result", mapString, "DOES NOT match expected:", expected)
 	}
 }
+
+func TestAnyMapStringInterfaceToMapStringString(t *testing.T) {
+	mapInterface := map[string]any{
+		"first_name": "John",
+		"last_name":  "Doe",
+	}
+
+	mapString := AnyToMapStringString(mapInterface)
+
+	expected := map[string]string{"first_name": "John", "last_name": "Doe"}
+
+	if !reflect.DeepEqual(mapString, expected) {
+		t.Error("Result", mapString, "DOES NOT match expected:", expected)
+	}
+}
